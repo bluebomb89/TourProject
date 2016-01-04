39cbc2668528413a33962c710eb0c3bffc3be592
sist <sist@sist>
2016-01-04 15:06:41 +0900

    카카오톡 로그인 에러 수정

--- src/main/webapp/js/main/kakaotalk.js
+++ src/main/webapp/js/main/kakaotalk.js
@@ -13,7 +13,7 @@ console.log(error);
 });
 }
 function createKakaotalkLogin(){
-$("#hnplogin .kakao-logout-btn,#hnplogin .kakao-login-btn").remove();
+$("#hnplogin .kakao-logout-btn,#hnplogin .kakao-login-btn,#kakao-logged-group .kakao-logout-btn,#kakao-logged-group .kakao-login-btn").remove();
 var loginBtn = $("<a/>",{"class":"kakao-login-btn","text":"카카오 톡으로 로그인하기"});
 var mainloginBtn = $("<a/>",{"class":"kakao-login-btn mbr-buttons__link btn text-white","text":"로그인","data-toggle":"modal","data-target":".bs-example-modal-lg"});
 loginBtn.click(function(){
@@ -31,10 +31,11 @@ console.log(err);
 });
 $("#hnplogin").prepend(loginBtn)
 $("#kakao-logged-group").prepend(mainloginBtn)
+
 }
 function createKakaotalkLogout(){
 $("#kakao-logged-group .kakao-logout-btn,#kakao-logged-group .kakao-login-btn").remove();
-var logoutBtn = $("<a/>",{"class":"kakao-logout-btn mbr-buttons__link btn text-white","text":"로그아웃","data-toggle":"modal"});
+var logoutBtn = $("<a/>",{"class":"kakao-logout-btn mbr-buttons__link btn text-white","text":"로그아웃"});
 logoutBtn.click(function(){
 Kakao.Auth.logout(); 
 createKakaotalkLogin();
